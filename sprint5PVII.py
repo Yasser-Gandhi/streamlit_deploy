4d4b771ce1c391f4400a858acfc43a18f2c30832
cesargafer <cesargafer@gmail.com>
2024-03-24 14:25:48 -0600

    Historia ID # 7 - Extensión de interfaz
    En el sidebar del dashboard coloca un componente de texto y un botón de comando y realizar las siguientes
    operaciones:
    -Buscar los filmes por título.
    -El tipo de coincidencia será del tipo “que contenga”.
    -Deberá buscar filmes sin importar que se escriban en minúsculas o mayúsculas.

---
+++ sprint5PVII.py
@@ -0,0 +1,44 @@
+# 1. Crear el componente selectbox en el sidebar para cargar la columna 'director'
+directores = df_todos_los_filmes_['director'].unique()
+selected_director = st.sidebar.selectbox("Seleccionar Director", directores)
+
+
+# Función para insertar un nuevo filme en Firestore
+def insertar_filme(nombre, director, genero, compañia):
+    # Crear un documento en la colección 'peliculas' con los datos proporcionados
+    db.collection(u'peliculas').add({
+        u'name': nombre,
+        u'director': director,
+        u'genre': genero,
+        u'company': compañia
+    })
+    st.success("El filme se ha insertado correctamente en la base de datos.")
+    # Limpiar los campos del formulario después de insertar el filme
+    nombre_input.text_input = ""
+    director_input.text_input = ""
+    genero_select.selectbox = ""
+    compañia_input.text_input = ""
+
+# Interfaz de Streamlit
+st.sidebar.title("Agregar Nuevo Filme")
+
+# Crear contenedores para los controles del formulario
+form_container = st.sidebar.beta_container()
+
+# Controles para ingresar los datos del nuevo filme
+with form_container:
+    st.subheader("Ingrese los datos del nuevo filme")
+    nombre_input = st.text_input("Nombre del Filme")
+    director_input = st.text_input("Director")
+    genero_select = st.selectbox("Género", ["Acción", "Comedia", "Drama", "Ciencia Ficción", "Suspenso", "Animación", "Documental"])
+    compañia_input = st.text_input("Compañía Productora")
+    submit_button = st.form_submit_button("Insertar Filme")
+
+# Botón para insertar el nuevo filme
+if submit_button:
+    # Validar que se hayan ingresado todos los datos
+    if nombre_input.strip() and director_input.strip() and genero_select.strip() and compañia_input.strip():
+        # Llamar a la función para insertar el nuevo filme en Firestore
+        insertar_filme(nombre_input, director_input, genero_select, compañia_input)
+    else:
+        st.warning("Por favor ingresa todos los datos del filme.")
